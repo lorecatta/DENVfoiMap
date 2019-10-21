77689f0d2ebc509ad116ef34d0583ea1659ea455
Lorenzo Cattarino <lcattari@dide.local>
2019-10-21 11:03:27 +0100

    dataset description

---
+++ R/data.R
@@ -0,0 +1,110 @@
+#' Covariates at admin unit 1 level.
+#'
+#' A dataset containing global environmental and demographic variables at
+#' administrative unit 1 resolution.
+#'
+#' @format a data frame with 3305 rows and 37 columns
+"admin_covariates"
+
+
+#' Countries' age structure
+#'
+#' A dataset containing the proportion of individuals in 20 5-year age groups for 200 countries.
+#'
+#' @format a data frame with 200 rows and 28 columns
+"age_structure"
+
+
+#' Force of infection estimates and covariates at admin unit 1.
+#'
+#' A dataset containing FOI estimates and environmental and demographic covariates at
+#' administrative unit 1 resolution.
+#'
+#' @format a data frame with 693 rows and 49 columns
+"foi_data"
+
+
+#' Force of infection estimates and covariates at 1/6 degree.
+#'
+#' A dataset containing FOI estimates and environmental and demographic covariates at
+#' 1/6 degree resolution.
+#'
+#' @format a data frame with 425138 rows and 43 columns
+"all_sqr_covariates"
+
+
+#' Dengue endemic countries.
+#'
+#' A dataset containing GADM identifier fields for country and administrative unit 1
+#' where dengue is an endemic disease.
+#'
+#' @format a data frame with 1952 rows and 3 columns
+"endemic_ID_0_ID_1"
+
+
+#' Selected predictors.
+#'
+#' A dataset containing the top explanatory variables as output from variable sslection.
+#'
+#' @format a data frame with 28 rows and 3 columns
+"predictor_rank"
+
+
+#' Look up table of vaccine impact on mild cases (R0 assumption 1).
+#'
+#' A dataset containing the fraction of mild febbrile cases averted following application of the
+#' Sanofi Pasteur dengue vaccine, when assuming only primary and secondary
+#' infections are infectious, for different R0 values.
+#'
+#' @format a data frame with 17 rows and 19 columns
+"R0_to_prop_cases_averted_lookup_1"
+
+
+#' Look up table of vaccine impact on mild cases (R0 assumption 2).
+#'
+#' A dataset containing the fraction of mild febbrile cases averted following application of the
+#' Sanofi Pasteur dengue vaccine, when assuming all 4 infections are infectious,
+#' for different R0 values.
+#'
+#' @format a data frame with 17 rows and 19 columns
+"R0_to_prop_cases_averted_lookup_2"
+
+
+#' Look up table of vaccine impact on hospitalized cases (R0 assumption 1).
+#'
+#' A dataset containing the fraction of hospitalized cases averted following application of the
+#' Sanofi Pasteur dengue vaccine, when assuming only primary and secondary
+#' infections are infectious, for different R0 values.
+#'
+#' @format a data frame with 17 rows and 19 columns
+"R0_to_prop_hosp_averted_lookup_1"
+
+
+#' Look up table of vaccine impact on hospitalized cases (R0 assumption 2).
+#'
+#' A dataset containing the fraction of hospitalized cases averted following application of the
+#' Sanofi Pasteur dengue vaccine, when assuming all 4 infections are infectious,
+#' for different R0 values.
+#'
+#' @format a data frame with 17 rows and 19 columns
+"R0_to_prop_hosp_averted_lookup_2"
+
+
+#' Look up table of vaccine impact on infections (R0 assumption 1).
+#'
+#' A dataset containing the fraction of infections averted following application of the
+#' Sanofi Pasteur dengue vaccine, when assuming only primary and secondary
+#' infections are infectious, for different R0 values.
+#'
+#' @format a data frame with 17 rows and 19 columns
+"R0_to_prop_infections_averted_lookup_1"
+
+
+#' Look up table of vaccine impact on infections (R0 assumption 2).
+#'
+#' A dataset containing the fraction of infections averted following application of the
+#' Sanofi Pasteur dengue vaccine, when assuming all 4 infections are infectious,
+#' for different R0 values.
+#'
+#' @format a data frame with 17 rows and 19 columns
+"R0_to_prop_infections_averted_lookup_2"
