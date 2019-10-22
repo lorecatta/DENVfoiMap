795a849c60c68ab9933ed8138093ef03ba4b60d5
Lorenzo Cattarino <lcattari@dide.local>
2019-10-22 15:56:11 +0100

    remove unused roxygen documentation

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,17 +1,4 @@
 
-#------------------------------------------------------------------------------
-
-# The function allows to do parallel computation using the \code{\link{parallel}} package
-#
-# @title Wrapper for parallel computation
-#
-# @param ... aerguement passed on to \code{lapply} or \code{parallel::parLapply}.
-#
-# @param parallel logicla indicating whether to run things in parallel.
-#
-# @export
-
-
 loop <- function(..., parallel) {
   if (parallel) {
     parallel::parLapply(NULL, ...)
