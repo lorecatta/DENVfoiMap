9197c3c55e6970483c3617c25e8950d18b68ce1a
Lorenzo Cattarino <lcattari@dide.local>
2019-10-22 14:58:51 +0100

    update

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,15 +1,15 @@
 
 #------------------------------------------------------------------------------
 
-#' The function allows to do parallel computation using the \code{\link{parallel}} package
-#'
-#' @title Wrapper for parallel computation
-#'
-#' @param ... aerguement passed on to \code{lapply} or \code{parallel::parLapply}.
-#'
-#' @param parallel logicla indicating whether to run things in parallel.
-#'
-#' @export
+# The function allows to do parallel computation using the \code{\link{parallel}} package
+#
+# @title Wrapper for parallel computation
+#
+# @param ... aerguement passed on to \code{lapply} or \code{parallel::parLapply}.
+#
+# @param parallel logicla indicating whether to run things in parallel.
+#
+# @export
 
 
 loop <- function(..., parallel) {
