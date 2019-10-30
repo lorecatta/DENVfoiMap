95b4bce0b140cf2d3ab63cc1aaf5cfe41652beb8
Lorenzo Cattarino <lcattari@dide.local>
2019-10-30 10:44:40 +0000

    add roxygen documentation

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,4 +1,18 @@
 
+
+#------------------------------------------------------------------------------
+
+#' The function is a wrapper to lapply and parLapply.
+#'
+#' @title Parallel computation wrapper
+#'
+#' @param ... aaditional arguments passed on to lapply or parLapply.
+#'
+#' @param parallel logical. Is parLapply used?
+#'
+#' @export
+
+
 loop <- function(..., parallel) {
   if (parallel) {
     parallel::parLapply(NULL, ...)
