36048ea51cb2dec33fe947eb29ac3ff825525ab4
Lorenzo Cattarino <lcattari@dide.local>
2019-11-01 14:24:23 +0000

    it works finally

--- analysis.R
+++ analysis.R
@@ -14,9 +14,10 @@ extra_prms <- list(id_fld = "unique_id",
                    grp_flds = c("unique_id", "ID_0", "ID_1"),
                    ranger_threads = NULL,
                    fit_type = "boot",
-                   parallel_2 = TRUE,
+                   parallel_2 = FALSE,
                    screening_ages = c(9, 16),
-                   target_nm = c("I", "C", "HC", "R0_1", "R0_2"))
+                   target_nm = c("I", "C", "HC", "R0_1", "R0_2"),
+                   coord_limits = c(-74, -32, -34, 6))
 
 my_col <- colorRamps::matlab.like(100)
 
@@ -33,6 +34,10 @@ all_wgt <- parameters$all_wgt
 all_predictors <- predictor_rank$name
 
 base_info <- parameters$base_info
+
+foi_offset <- parameters$foi_offset
+
+coord_limits <- parameters$coord_limits
 
 screening_ages <- parameters$screening_ages
 
@@ -53,6 +58,7 @@ foi_data[foi_data$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
 foi_data_bsample <- grid_and_boot(data_df = foi_data,
                                   parms = parameters)
 
+# this RF fitting is going to take 20-30 minutes
 RF_obj_optim <- full_routine_bootstrap(parms = parameters,
                                        original_foi_data = foi_data,
                                        adm_covariates = admin_covariates,
@@ -60,23 +66,28 @@ RF_obj_optim <- full_routine_bootstrap(parms = parameters,
                                        covariates_names = all_predictors,
                                        boot_sample = foi_data_bsample[[1]])
 
-global_predictions <- make_ranger_predictions(RF_obj_optim,
-                                              dataset = all_sqr_covariates,
+# keep only pixels in Brazil as example
+BRA_sqr_covariates <- all_sqr_covariates[all_sqr_covariates$ID_0 %in% 33,]
+
+BRA_predictions <- make_ranger_predictions(RF_obj_optim,
+                                              dataset = BRA_sqr_covariates,
                                               covariates_names = all_predictors)
 
-all_sqr_covariates$p_i <- global_predictions
-#saveRDS(all_sqr_covariates, "prediction.rds")
-all_sqr_covariates <- readRDS("prediction.rds")
-all_sqr_covariates_sub <- dplyr::inner_join(all_sqr_covariates, endemic_ID_0_ID_1)
+BRA_predictions <- BRA_predictions - foi_offset
+BRA_predictions[BRA_predictions < 0] <- 0
+
+BRA_sqr_covariates$p_i <- BRA_predictions
 
 # map
 mp_nm <- "FOI.png"
-quick_raster_map(pred_df = all_sqr_covariates_sub,
-                 statistic = "p_i",
+map_data_df <- map_preprocess(BRA_sqr_covariates, "p_i", parameters)
+quick_raster_map(countries = Brazil_contour,
+                 pred_r_df = map_data_df,
                  my_col = my_col,
+                 ttl = "FOI",
+                 parms = parameters,
                  out_pt = map_path,
-                 out_name = mp_nm,
-                 key_ttl = "FOI")
+                 out_name = mp_nm)
 
 
 
@@ -94,12 +105,6 @@ age_band_bnds <- drep::get_age_band_bounds(age_band_tgs)
 age_band_L_bounds <- age_band_bnds[, 1]
 age_band_U_bounds <- age_band_bnds[, 2]
 
-# set up a context which integrates nicely the parallel package
-ctx <- context::context_save(path = "context",
-                             package_sources = provisionr::package_sources(github = "lorecatta/DENVfoiMap"))
-context::context_load(ctx)
-context::parallel_cluster_start(7, ctx)
-
 # create lookup tables
 lookup_tabs <- create_lookup_tables(
   age_struct = age_structure,
@@ -113,7 +118,7 @@ sf_val <- parameters$sf_vals[1]
 
 # attach look up table id
 sqr_preds_2 <- dplyr::inner_join(age_structure[, c("age_id", "ID_0")],
-                                 all_sqr_covariates_sub,
+                                 BRA_sqr_covariates,
                                  by = "ID_0")
 
 sqr_preds_3 <- as.matrix(sqr_preds_2)
@@ -128,31 +133,32 @@ burden_estimates_raw <- wrapper_to_replicate_R0_and_burden(
   FOI_to_R0_2_list = lookup_tabs[[5]],
   parms = parameters)
 
-context::parallel_cluster_stop()
-
 burden_estimates <- post_processing_burden(sqr_preds_3, burden_estimates_raw, parameters)
 
 # map the R0 (assumption 1, only primary and secondary infections are infectious)
 
 mp_nm <- "R0_1.png"
-quick_raster_map(pred_df = burden_estimates,
-                 statistic = "transformed_1",
+map_data_df <- map_preprocess(burden_estimates, "transformed_1", parameters)
+quick_raster_map(countries = Brazil_contour,
+                 pred_r_df = map_data_df,
                  my_col = my_col,
+                 ttl = expression("R"[0]),
+                 parms = parameters,
                  out_pt = map_path,
-                 out_name = mp_nm,
-                 z_range = seq(1, 15, 2),
-                 key_ttl = expression("R"[0]))
+                 out_name = mp_nm)
 
 # map the incidence of infections (per 1000)
 
 burden_estimates$I_num_inc <- burden_estimates$I_num / burden_estimates$population * 1000
 mp_nm <- "Infections.png"
-quick_raster_map(pred_df = burden_estimates,
-                 statistic = "I_num_inc",
+map_data_df <- map_preprocess(burden_estimates, "I_num_inc", parameters)
+quick_raster_map(countries = Brazil_contour,
+                 pred_r_df = map_data_df,
                  my_col = my_col,
+                 ttl = "Infections",
+                 parms = parameters,
                  out_pt = map_path,
-                 out_name = mp_nm,
-                 key_ttl = "Infections")
+                 out_name = mp_nm)
 
 
 
@@ -170,8 +176,6 @@ quick_raster_map(pred_df = burden_estimates,
 # assume a transmission reduction effect of 30%
 sf_val <- parameters$sf_vals[4]
 
-context::parallel_cluster_start(7, ctx)
-
 tr_red_impact_estimates_raw <- wrapper_to_replicate_R0_and_burden(
   foi_data = sqr_preds_3,
   scaling_factor = sf_val,
@@ -182,20 +186,20 @@ tr_red_impact_estimates_raw <- wrapper_to_replicate_R0_and_burden(
   FOI_to_R0_2_list = lookup_tabs[[5]],
   parms = parameters)
 
-context::parallel_cluster_stop()
-
 tr_red_impact_estimates <- post_processing_burden(sqr_preds_3, tr_red_impact_estimates_raw, parameters)
 
 # map the incidence of infections (per 1000) - assumption 1
 
 tr_red_impact_estimates$I_num_1_inc <- tr_red_impact_estimates$I_num_1 / tr_red_impact_estimates$population * 1000
 mp_nm <- "Infections_30pc_tr_red_impact.png"
-quick_raster_map(pred_df = tr_red_impact_estimates,
-                 statistic = "I_num_1_inc",
+map_data_df <- map_preprocess(tr_red_impact_estimates, "I_num_1_inc", parameters)
+quick_raster_map(countries = Brazil_contour,
+                 pred_r_df = map_data_df,
                  my_col = my_col,
+                 ttl = "Infections",
+                 parms = parameters,
                  out_pt = map_path,
-                 out_name = mp_nm,
-                 key_ttl = "Infections")
+                 out_name = mp_nm)
 
 
 # vaccine ---------------------------------------------------------------------
@@ -214,17 +218,17 @@ prop_averted <- wrapper_to_replicate_vaccine_impact(R0_preds = R0_1_preds,
                                                     screen_age = screen_age,
                                                     parms = parameters)
 
-prop_averted <- do.call("rbind", output1)
-
 burden_net_vaccine <- (1 - prop_averted[[1]]) * burden_estimates[, "I_num"]
 
-burden_estimates <- cbind(burden_estimates[, base_info], I_vacc_impact = burden_net_vaccine)
+burden_estimates_2 <- cbind(burden_estimates[, base_info], I_vacc_impact = burden_net_vaccine)
 
-burden_estimates$I_num_1_inc_v <- burden_estimates$I_vacc_impact / burden_estimates$population * 1000
+burden_estimates_2$I_num_1_inc_v <- burden_estimates_2$I_vacc_impact / burden_estimates_2$population * 1000
 mp_nm <- "Infections_vacc_9yr_impact.png"
-quick_raster_map(pred_df = burden_estimates,
-                 statistic = "I_num_1_inc_v",
+map_data_df <- map_preprocess(burden_estimates_2, "I_num_1_inc_v", parameters)
+quick_raster_map(countries = Brazil_contour,
+                 pred_r_df = map_data_df,
                  my_col = my_col,
+                 ttl = "Infections",
+                 parms = parameters,
                  out_pt = map_path,
-                 out_name = mp_nm,
-                 key_ttl = "Infections")
+                 out_name = mp_nm)
